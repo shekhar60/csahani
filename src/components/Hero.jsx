3b936f1199f1a1bf5f593cbd45830dc718f714d8
Chandrashekhar Sahani <shekhar60@users.noreply.github.com>
2025-08-20 13:39:35 +0530

    profile pic changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import profilePic from "../assets/csprofile.webp";
+import profilePic from "../assets/profilepic.jpeg";
 import {HERO_CONTENT} from "../constants";
 import { motion } from "framer-motion";
 
